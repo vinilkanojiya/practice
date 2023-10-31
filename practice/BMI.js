cb445f1cb376d3df18ef9ad1a8aebc74084e27b3
vinil kanojiya <41778036+vinilkanojiya@users.noreply.github.com>
2023-11-01 01:55:12 +0530

    Update BMI.js

--- practice/BMI.js
+++ practice/BMI.js
@@ -1,7 +1,20 @@
-function bmiCalculator(weight, height)
-{
-    return Math.round(weight/(height*height));
+function bmiCalculator (weight, height) {
+    var bmi = weight / (Math.pow(height,2));
+    var interpretation;
+    if (bmi<18.5)
+    {
+     interpretation = "Your BMI is " + bmi + ", so you are underweight.";   
+    }
+    else{
+       
+        if(bmi>18.5 && bmi<24.9)
+        {
+                interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";   
+        }
+        else
+        {
+                interpretation = "Your BMI is " + bmi + ", so you are overweight.";   
+        }
+    }
+    return interpretation;
 }
-
-var bmi = bmiCalculator(65, 1.8); 
-console.log(bmi);
