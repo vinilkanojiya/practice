341f72b33605a87bb16ee59f079265ae0e0bd33c
vinil kanojiya <41778036+vinilkanojiya@users.noreply.github.com>
2023-11-01 01:26:15 +0530

    Create LoveCal.js

---
+++ practice/LoveCal.js
@@ -0,0 +1,8 @@
+var firstName = prompt("what is your name?");        
+var secondName = prompt("what is your partner's name?");
+
+var num = (Math.random())*100;
+console.log(num);
+var percent = Math.floor(num)+1;
+console.log(percent);
+alert("Your laove percent is" + percent + "%");
