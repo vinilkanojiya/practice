03fe19fab733fbe60f40d76fbd245922a1f547b8
vinil kanojiya <41778036+vinilkanojiya@users.noreply.github.com>
2023-11-01 17:56:32 +0530

    Update fizzBuzz.js

--- practice/fizzBuzz.js
+++ practice/fizzBuzz.js
@@ -1,3 +1,4 @@
+
 var output = [];
 var num = 0;
 
@@ -26,3 +27,31 @@ num++;
  console.log(output);
     
 }
+
+
+//by loop
+var output= [];
+var num = 1;
+while(num<101)
+{
+     if(num%3==0 && num%5==0)
+    {
+        output.push('fizzbuzz');
+    }
+   else if(num%3==0)
+    {
+            output.push('fizz');
+    }
+    else if(num%5==0)
+     {
+                output.push('buzz');
+     }
+      else
+      {
+            output.push(num);   
+      }
+
+    num++;        
+}
+
+console.log(output);
