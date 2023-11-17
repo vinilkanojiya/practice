613eeca52b868d63731988668d3cd20f46e3cf7a
vinil kanojiya <41778036+vinilkanojiya@users.noreply.github.com>
2023-11-18 00:16:25 +0530

    Create higher Order function.js

---
+++ practice/higher Order function.js
@@ -0,0 +1,26 @@
+function add(num1, num2){
+    return num1+num2;
+}
+
+function substract(num1, num2)
+{
+    return num1-num2;
+}
+
+function multiply(num1,num2)
+{
+    return num1*num2;
+}
+
+function divide(num1,num2)
+{
+    return num1/num2;        
+}
+
+function calculator(num1,num2,operator)
+{
+    return operator(num1,num2);
+}
+
+var num = calculator(2,3,substract);
+console.log("ans" + num);
